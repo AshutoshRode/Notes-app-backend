6f07dd087d755476d6887a1f83552af4c8a983e6
Ashutosh Vilas Rode <117566964+AshutoshRode@users.noreply.github.com>
2025-07-25 19:45:40 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,18 +1,32 @@
+// const mysql2 = require('mysql2');
+
+// const pool = mysql2.createPool({
+//     host: 'sql12.freesqldatabase.com',
+//     user: 'sql12788691',
+//     password: 'pZkGFRa3QN',
+//     database: 'sql12788691',
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0
+// });
+// module.exports = pool 
+
+
 const mysql2 = require('mysql2')
 
 const pool= mysql2.createPool({
     
-    host :'sql12.freesqldatabase.com',
-    user:'sql12788691',
-    password:'pZkGFRa3QN',
+    host :'bbyvzst3ooow4c2xbtbg-mysql.services.clever-cloud.com',
+    user:'ubsm6crero1e4lvh',
+    password:'jtAjOshgPNDXP0bQf1O5',
     port:3306,
-    database:'sql12788691',
+    database:'bbyvzst3ooow4c2xbtbg',
     waitForConnections:true,
     connectionLimit:10,
     queueLimit:0
 })
- module.exports = pool 
 
+module.exports = pool 
 
 // const mysql2 = require('mysql2')
 
