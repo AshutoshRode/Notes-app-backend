87084df6407116e5c7348ff43e780659be68c4e3
Ashutosh Vilas Rode <117566964+AshutoshRode@users.noreply.github.com>
2025-07-25 20:17:05 +0530

    Update mailer.js

--- mailer.js
+++ mailer.js
@@ -1,21 +1,22 @@
-const nodemailer = require('nodemailer')
-const fs = require('fs')
+const nodemailer = require('nodemailer');
+const fs = require('fs');
 
-function sendEmail(template, subject, email, callback) {
-
+function sendEmail(template, subject, email, replacements, callback) {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
             user: 'ashutoshmusic1999@gmail.com',
-            pass: 'ftpvvxbagyyuuonq'
-            // pass: 'jayshreeram@2022',
-
+            pass: 'ftpvvxbagyyuuonq',
         },
+    });
 
-    })
+    let contents = '' + fs.readFileSync('./email_templates/' + template);
 
-    const contents = '' + fs.readFileSync('./email_templates/' + template)
-    // console.log(contents)
+    // replace placeholders in template (e.g., {{VERIFICATION_LINK}})
+    for (const key in replacements) {
+        const placeholder = `{{${key}}}`;
+        contents = contents.replace(new RegExp(placeholder, 'g'), replacements[key]);
+    }
 
     const mailOptions = {
         from: 'ashutoshmusic1999@gmail.com',
@@ -25,10 +26,10 @@ function sendEmail(template, subject, email, callback) {
     };
 
     transporter.sendMail(mailOptions, function (error, info) {
-        callback(error, info)
+        callback(error, info);
     });
 }
 
 module.exports = {
     sendEmail: sendEmail,
-} 
+};
